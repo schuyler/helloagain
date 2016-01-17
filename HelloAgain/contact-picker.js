f247514ef4807d911bd593b350c4dfa635e768d0
Schuyler Erle <schuyler@nocat.net>
2016-01-16 18:32:31 -0800

    Add HAContactPickerView class for choosing contacts.
    Also, add famous astronomers to a contacts fixture modeled on the
    react-native-contacts API.

---
+++ HelloAgain/contact-picker.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import { CONTACT_FIXTURE } from './contact-fixture';
+import React, {
+  Component,
+  Text,
+  View,
+  ListView,
+  PropTypes,
+  StyleSheet
+} from 'react-native';
+
+export class HAContactPickerView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1.recordID != row2.recordID,
+      })
+    };
+  }
+
+  componentWillMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(CONTACT_FIXTURE)
+    });
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderContact}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderContact(contact) {
+    return (
+      <View style={styles.contactRow}>
+        <Text style={styles.contactName}>{contact.givenName} {contact.familyName}</Text>
+      </View>
+    );
+  }
+}
+
+HAContactPickerView.propTypes = { state: PropTypes.object };
+HAContactPickerView.defaultProps = { state: {} };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+  contactRow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  contactName: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+});
+
+
