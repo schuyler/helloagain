4fd8f5dce864c048c4d84b45d5b6c9dd1ed58198
Schuyler Erle <schuyler@nocat.net>
2017-01-15 12:59:37 -0800

    Ensure that ContactList renderRow prop is passed to underlying ListView

--- HelloAgain/__tests__/components/contact-list.js
+++ HelloAgain/__tests__/components/contact-list.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme'
 
 import { CONTACT_FIXTURE } from "../fixtures/contacts"
 import ContactList from "../../components/contact-list"
@@ -10,4 +11,12 @@ it('renders a list of contacts', () => {
   expect(renderer.create(
     <ContactList items={CONTACT_FIXTURE} />
   )).toMatchSnapshot();
+})
+
+it('accepts a renderRow prop', () => {
+  const renderRow = () => {}
+  const wrapper = shallow(
+    <ContactList items={CONTACT_FIXTURE} renderRow={renderRow} />
+  )
+  expect(wrapper.find("ListView").prop("renderRow")).toBe(renderRow)
 })
