eb7e122efcc79670d8934ba3f0a9d60a9f001fdc
Schuyler Erle <schuyler@nocat.net>
2017-01-14 23:01:32 -0800

    Unpack actions and action types.
    Also, use Jest snapshots to capture expected action structure.

--- HelloAgain/actions/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Enumerate the dispatchable actions.
-export const UPDATE_FRIEND = 'UPDATE_FRIEND'
-
-// Helpers for dispatching actions.
-export const updateFriend = (friend) => {
-  return { type: UPDATE_FRIEND, friend: friend }
-}
---
+++ HelloAgain/actions/types.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// Contacts
+export const TOGGLE_ACTIVE = 'TOGGLE_ACTIVE'
+
+// Friends
+export const UPDATE_FRIEND = 'UPDATE_FRIEND'
