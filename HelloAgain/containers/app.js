bb6624c307470b98082b776b235eeed2f552df8a
Schuyler Erle <schuyler@nocat.net>
2017-01-15 22:16:27 -0800

    Add NavigatorIOS component to root app. Yes, this will mean some untangling when we try to make Android work.

--- HelloAgain/containers/app.js
+++ HelloAgain/containers/app.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import { NavigatorIOS } from 'react-native';
+
 import store from '../store'
 import ContactPicker from './contact-picker'
 
@@ -8,11 +10,37 @@ export default class HelloAgain extends Component {
     super(props)
   }
 
+  /*
+   * stashing this here for future use:
+   *
+   pickContacts() {
+      const nav = this.refs.nav;
+      nav.push({
+        title: "Add Friends",
+        component: ContactPicker,
+        rightButtonTitle: null,
+        leftButtonTitle: "Back",
+        onLeftButtonPress: () => { nav.pop() }
+      })
+    }
+  */
+
   render() {
+    const initialRoute = {
+      title: "Hello Again!",
+      component: ContactPicker
+    }
     return (
       <Provider store={store}>
-        <ContactPicker />
+        <NavigatorIOS
+          ref="nav"
+          style={{flex: 1}}
+          initialRoute={initialRoute}
+          rightButtonTitle="👥"
+          leftButtonTitle="⚙"
+          /* onRightButtonPress={this.pickContacts} */
+        />
       </Provider>
-    )
+    );
   }
 }
