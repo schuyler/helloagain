95009dde1fa9020e30f24fb77206023c20c4df75
Schuyler Erle <schuyler@nocat.net>
2017-01-17 01:59:05 -0800

    Persist store to AsyncStorage using redux-persist.
    Implements a redux-persist "transform" filter that prevents persisting
    contact entries in the friend store which have never been activated in
    the app. This way we don't lug around the entire contact database; just
    the contacts the user cares enough to have clicked on at some point.

---
+++ HelloAgain/store/filter.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import { createTransform } from 'redux-persist'
+
+// onlyActivatedFriends allows us to scrub the friends state of contacts who
+// have never been active friends, before we persist the redux store. This
+// keeps us from pointlessly persisting the entire native contact database.
+
+export const onlyActivatedFriends = createTransform((state, key) => {
+  if (key !== 'friends') {
+    return state
+  }
+  let filtered = {}
+  Object.values(state)
+    .filter(f => f.isActive !== undefined)
+    .forEach((f) => {filtered[f.recordID] = f})
+  return filtered
+})
---
+++ HelloAgain/store/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import { createStore, applyMiddleware, compose } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
+import thunk from 'redux-thunk'
+import { AsyncStorage } from 'react-native'
+import { onlyActivatedFriends } from './filter'
+import mainReducer from '../reducers'
+
+// Purely for testing new components. Will be removed later.
+// import initialState from './store-scaffold'
+
+const enhancers = compose(
+  applyMiddleware(thunk),
+  autoRehydrate()
+)
+const store = createStore(mainReducer, enhancers)
+
+const config = {
+  storage: AsyncStorage,
+  transforms: [onlyActivatedFriends]
+}
+persistStore(store, config, () => {
+  const state = store.getState()
+  const friendCount = Object.values(state.friends).length
+  console.log(`Loaded ${friendCount} friends from AsyncStorage`)
+})
+
+export default store
