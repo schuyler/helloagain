cd05078e23436236edf9f0b18f756407f4a70324
Schuyler Erle <schuyler@nocat.net>
2017-01-17 22:05:58 -0800

    Add shared styling.

---
+++ HelloAgain/components/shared.js
@@ -0,0 +1,31 @@
+'use strict';
+
+export default {
+  row: {
+    flex: 1,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  rowHighlight: {
+    underlayColor: 'dodgerblue',
+    activeOpacity: 0.2
+  },
+  rowPicture: {
+    flex: 1,
+    width: 32,
+    height: 32
+  },
+  rowName: {
+    textAlign: 'left',
+    fontSize: 20,
+    flex: 5
+  },
+  rowProps: {
+    fontSize: 24,
+    textAlign: 'right',
+    marginRight: 5,
+    flex: 1
+  }
+}
